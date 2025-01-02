927a66b84ec62e1d2a6b0524bcfb089abbc8f247
kyyogaa4 <74727542+kyyogaa4@users.noreply.github.com>
2025-01-02 23:21:09 +0100

    Fix Errors in Advanced Search Syntax

--- prepack.js
+++ prepack.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
-
+ wjSxYh8d4v
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
