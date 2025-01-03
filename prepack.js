ebff6e480500d7cea021a6cde4190a40ff879b89
kyyogaa4 <74727542+kyyogaa4@users.noreply.github.com>
2025-01-03 21:01:45 +0100

    Add advanced search filters for reports

--- prepack.js
+++ prepack.js
@@ -5,5 +5,5 @@ var pkg = JSON.parse(fs.readFileSync(
 , 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
-
+ FRd2xNw28s
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
